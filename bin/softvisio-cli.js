499115b335c05b2d1a17d9462dc47584b80902e0
zdm <zdm@softvisio.net>
2021-02-11 17:45:19 +0200

    release command renamed to publish

--- bin/softvisio-cli.js
+++ bin/softvisio-cli.js
@@ -7,7 +7,7 @@ const Rpc = require( "@softvisio/cli/commands/rpc" );
 const Wiki = require( "@softvisio/cli/commands/wiki" );
 const Icons = require( "@softvisio/cli/commands/icons" );
 const Lint = require( "@softvisio/cli/commands/lint" );
-const Release = require( "@softvisio/cli/commands/release" );
+const Publish = require( "@softvisio/cli/commands/publish" );
 const Ls = require( "@softvisio/cli/commands/ls" );
 const Log = require( "@softvisio/cli/commands/log" );
 const Docker = require( "@softvisio/cli/commands/docker" );
@@ -23,7 +23,7 @@ class App {
                 "wiki": Wiki,
                 "icons": Icons,
                 "lint": Lint,
-                "release": Release,
+                "publish": Publish,
                 "ls": Ls,
                 "log": Log,
                 "docker": Docker,
