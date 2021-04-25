993596db9054e2228e8106736cc9935301bc6f3f
zdm <zdm@softvisio.net>
2021-04-25 18:16:07 +0300

    ported to esm

--- bin/softvisio-cli.js
+++ bin/softvisio-cli.js
@@ -7,12 +7,12 @@ import cli from "@softvisio/core/cli";
 import Rpc from "#lib/commands/rpc.cjs";
 import Wiki from "#lib/commands/wiki.cjs";
 import Icons from "#lib/commands/icons.cjs";
-import Lint from "#lib/commands/lint.cjs";
+import Lint from "#lib/commands/lint.js";
 import Publish from "#lib/commands/publish.cjs";
 import Ls from "#lib/commands/ls.cjs";
 import Log from "#lib/commands/log.cjs";
 import Docker from "#lib/commands/docker.cjs";
-import Git from "#lib/commands/git.cjs";
+import Git from "#lib/commands/git.js";
 import Status from "#lib/commands/status.cjs";
 
 class App {
