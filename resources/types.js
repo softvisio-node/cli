3398fe3bd2971b32769e2e416df86e25e1503909
zdm <zdm@softvisio.net>
2021-06-28 20:30:49 +0300

    docs types updated

--- resources/types.js
+++ resources/types.js
@@ -1,17 +1,17 @@
 export default {
 
-    // primitive types
-    "any": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#Data_types",
+    // javascript primitive types
+    "any": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#data_and_structure_types",
+    "boolean": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#boolean_type",
     "null": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#null_type",
     "undefined": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#undefined_type",
     "number": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#number_type",
     "integer": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#number_type",
     "bigint": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#bigint_type",
+    "string": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#string_type",
     "symbol": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#symbol_type",
-    "string": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#String_type",
-    "boolean": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#Boolean_type",
 
-    // global types
+    // global javascript types
     "Object": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object",
     "Array": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array",
     "Function": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function",
@@ -25,4 +25,7 @@ export default {
     // nodejs types
     "URL": "https://nodejs.org/api/all.html#url_the_whatwg_url_api",
     "Buffer": "https://nodejs.org/api/buffer.html#buffer_class_buffer",
+
+    // global types
+    "Result": "https://softvisio.github.io/result/#/?id=class-result",
 };
