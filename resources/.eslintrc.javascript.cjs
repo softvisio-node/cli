b76380f07968c30d183f8b87cc5800d66e371cb2
zdm <zdm@softvisio.net>
2023-05-04 06:56:43 +0300

    fix: eslint camel case rule options

--- resources/.eslintrc.javascript.cjs
+++ resources/.eslintrc.javascript.cjs
@@ -34,7 +34,15 @@ const config = {
     },
 
     "rules": {
-        "@softvisio/camelcase": ["error", { "properties": "never", "ignoreImports": true, "allowConsecutiveCapitalLetters": false, "allowedPrefixes": ["API_", "CALLBACK_", "COMMAND_"] }],
+        "@softvisio/camelcase": [
+            "error",
+            {
+                "properties": "never",
+                "ignoreImports": true,
+                "allowConsecutiveCapitalLetters": false,
+                "allowedPrefixes": ["API_", "CALLBACK_", "COMMAND_"],
+            },
+        ],
 
         // eslint comments
         "eslint-comments/no-unused-disable": "error",
