cd827c1d3e5f828cde4b3449df8709e3718ed72a
zdm <zdm@softvisio.net>
2024-01-24 11:40:49 +0200

    fix: linted

--- docs/assets/theme/theme.js
+++ docs/assets/theme/theme.js
@@ -88,7 +88,7 @@ class Theme {
 
             blocks[n] = blocks[n].replaceAll( /<([\w.]+)(\[\])?\\>/g, ( match, type, array ) => {
                 if ( type in types ) {
-                    return `<[${type}${array ?? ""}](${types[type]})\\>`;
+                    return `<[${ type }${ array ?? "" }](${ types[type] })\\>`;
                 }
                 else {
                     return match;
