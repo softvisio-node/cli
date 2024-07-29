3ea5e5f4361db220b7f90e10bbc6b45d3c304341
zdm <zdm@softvisio.net>
2024-07-30 02:09:10 +0300

    fix: tests

--- _tests/001-git.test.js
+++ _tests/001-git.test.js
@@ -3,27 +3,29 @@
 import { describe, test } from "node:test";
 import assert from "node:assert";
 
-describe( "git-branch-parser", () => {
-    const BRANCH_RE = /^(?<current>\*)? +(?:\((?<head>HEAD)[^)]+\)|(?<branch>[^ ]+)) +(?<hash>[a-z0-9]+)(?: \[ahead (?<ahead>\d+)\])? (?<description>.+)/;
+describe( "git", () => {
+    describe( "git-branch-parser", () => {
+        const BRANCH_RE = /^(?<current>\*)? +(?:\((?<head>HEAD)[^)]+\)|(?<branch>[^ ]+)) +(?<hash>[a-z0-9]+)(?: \[ahead (?<ahead>\d+)\])? (?<description>.+)/;
 
-    const tests = [
+        const tests = [
 
-        //
-        [ `  main fa469cf [ahead 1] feat: docker add git labels on build`, { "branch": "main", "ahead": "1" } ],
-        [ `* (HEAD detached at latest) a938d78 [ahead 10] release: v0.1.7`, { "branch": undefined, "head": "HEAD", "ahead": "10" } ],
-        [ `  main fa469cf feat: docker add git labels on build`, { "branch": "main", "ahead": undefined } ],
-        [ `* (HEAD detached at latest) a938d78 release: v0.1.7`, { "branch": undefined, "head": "HEAD", "ahead": undefined } ],
-    ];
+            //
+            [ `  main fa469cf [ahead 1] feat: docker add git labels on build`, { "branch": "main", "ahead": "1" } ],
+            [ `* (HEAD detached at latest) a938d78 [ahead 10] release: v0.1.7`, { "branch": undefined, "head": "HEAD", "ahead": "10" } ],
+            [ `  main fa469cf feat: docker add git labels on build`, { "branch": "main", "ahead": undefined } ],
+            [ `* (HEAD detached at latest) a938d78 release: v0.1.7`, { "branch": undefined, "head": "HEAD", "ahead": undefined } ],
+        ];
 
-    for ( let n = 0; n < tests.length; n++ ) {
-        test( "test_" + n, () => {
-            const match = tests[ n ][ 0 ].match( BRANCH_RE );
+        for ( let n = 0; n < tests.length; n++ ) {
+            test( "test_" + n, () => {
+                const match = tests[ n ][ 0 ].match( BRANCH_RE );
 
-            if ( !match ) throw Error( `Parsing error: ` + tests[ n ][ 0 ] );
+                if ( !match ) throw Error( `Parsing error: ` + tests[ n ][ 0 ] );
 
-            for ( const prop in tests[ n ][ 1 ] ) {
-                assert.strictEqual( tests[ n ][ 1 ][ prop ], match.groups[ prop ] );
-            }
-        } );
-    }
+                for ( const prop in tests[ n ][ 1 ] ) {
+                    assert.strictEqual( tests[ n ][ 1 ][ prop ], match.groups[ prop ] );
+                }
+            } );
+        }
+    } );
 } );
