1f68512bc33fd407404a2ca0360eb04af5cf1659
zdm <zdm@softvisio.net>
2024-08-01 20:03:04 +0300

    feat: eslint 9

--- resources/eslint.config.typescript.js
+++ resources/eslint.config.typescript.js
@@ -1,10 +1,4 @@
 import baseConfig from "./eslint.config.javascript.js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-    ...baseConfig,
-
-    // ...tseslint.configs.recommended,
-    ...tseslint.configs.strict,
-    ...tseslint.configs.stylistic
-);
+export default tseslint.config( ...baseConfig, ...tseslint.configs.recommended );
