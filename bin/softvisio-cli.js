d6f0334608eb215a1ac529103b27916233cb974d
zdm <zdm@softvisio.net>
2024-08-03 21:16:42 +0300

    feat: commands structure redactor

--- bin/softvisio-cli.js
+++ bin/softvisio-cli.js
@@ -9,67 +9,56 @@ const spec = {
     "commands": {
         "docker": {
             "short": "D",
-            "title": "docker tools",
+            "title": "Docker tools",
             "module": () => new URL( "../lib/commands/docker.js", import.meta.url ),
         },
         "docs": {
             "short": "d",
-            "title": "documentation tools",
+            "title": "Documentation tools",
             "module": () => new URL( "../lib/commands/docs.js", import.meta.url ),
         },
         "generate": {
-            "short": "g",
-            "title": "generate code",
+            "title": "Generate data",
             "module": () => new URL( "../lib/commands/generate.js", import.meta.url ),
         },
         "git": {
-            "short": "G",
-            "title": "git tools",
+            "title": "GIT tools",
             "module": () => new URL( "../lib/commands/git.js", import.meta.url ),
         },
-        "icons": {
-            "title": "generate icons for cordova project",
-            "module": () => new URL( "../lib/commands/icons.js", import.meta.url ),
+        "package": {
+            "short": "p",
+            "title": "Package tools",
+            "module": () => new URL( "../lib/commands/package.js", import.meta.url ),
         },
         "lint": {
-            "title": "lint sources",
+            "short": "L",
+            "title": "Lint sources",
             "module": () => new URL( "../lib/commands/lint.js", import.meta.url ),
-        },
-        "localization": {
-            "title": "localization tools",
-            "module": () => new URL( "../lib/commands/localization.js", import.meta.url ),
         },
         "log": {
             "short": "l",
-            "title": "get changelog for unreleased changes",
+            "title": "Get changelog for unreleased changes",
             "module": () => new URL( "../lib/commands/log.js", import.meta.url ),
         },
         "ls": {
-            "title": "list projects in workspace",
+            "title": "List projects in workspace",
             "module": () => new URL( "../lib/commands/ls.js", import.meta.url ),
         },
-        "publish": {
-            "title": "release and publish the project",
-            "module": () => new URL( "../lib/commands/publish.js", import.meta.url ),
-        },
         "rpc": {
-            "title": "run RPC service",
+            "title": "Run RPC service",
             "module": () => new URL( "../lib/commands/rpc.js", import.meta.url ),
         },
         "status": {
-            "title": "prints project status",
+            "title": "Get project status",
             "module": () => new URL( "../lib/commands/status.js", import.meta.url ),
         },
-        "test": {
-            "title": "test suite",
-            "module": () => new URL( "../lib/commands/test.js", import.meta.url ),
-        },
         "wiki": {
-            "title": "wiki tools",
+            "title": "Wiki tools",
             "module": () => new URL( "../lib/commands/wiki.js", import.meta.url ),
         },
         "apt": {
-            "title": "apt repository tools",
+            "short": "a",
+            "title": "Apt repository tools",
             "module": () => new URL( "../lib/commands/apt.js", import.meta.url ),
         },
     },
